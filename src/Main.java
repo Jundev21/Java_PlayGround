d511305dd55fc4e492ef24f94e15006fd65786b5
jun <jundev21@gmail.com>
2023-08-02 10:37:28 +0900

    메인

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    static public class A{
+        private void test(){
+            System.out.println("protected modifier");
+        }
+
+        protected  void testB(){
+            System.out.println("it is protected");
+        }
+    }
+
+    static public class B extends A{
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
